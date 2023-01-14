6028b203b9055ef636fd6baadf2de1ec2e10529b
anastafish <anastafish24@gmail.com>
2023-01-14 22:11:41 +0300

    finished the cv application site!

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -5,18 +5,37 @@ function editForm(){
     props.setSubmit(false)
 }
 
+
+
+// companyName:"",
+// positionTitle:"",
+// mainTasks:"",
+// workingDateFrom:"",
+// workingDateTo:""    
     return(
         <div className="info-container">
-            <h1 className='info'>First Name: {props.form.firstName}</h1>
-            <h1 className='info'>Last Name: {props.form.lastName}</h1>   
-            <h1 className='info'>Email: {props.form.email}</h1>
-            <h1 className='info'>Study Period: <span className="date">{props.form.studyDateFro}</span> <span className="date">{props.form.studyDateTo}</span></h1>
-            <h1 className='info'>School Name: {props.form.schoolName}</h1>
-            <h1 className='info'>Study Title: {props.form.studyTitle}</h1>
-            <h1 className='info'>Company Name: {props.form.companyName}</h1>
-            <h1 className='info'>Position Title: {props.form.positionTitle}</h1>
-            <h1 className='info'>Main Tasks: {props.form.mainTasks}</h1>
-            <h1 className='info'>Working Period: From <span className="date">{props.form.workingDateFrom}</span> TO <span className="date">{props.form.workingDateTo}</span></h1>
+            <div className="form">
+                <div className="form-section">
+                    <h1>First Name: {props.form.firstName}</h1>
+                    <h1>LastName: {props.form.lastName}</h1>
+                </div>
+                <div className='form-section'>
+                    <h1>Email: {props.form.email}</h1>
+                    <h1>Phone Number: {props.form.phoneNumber}</h1>
+                </div>
+                <div className="form-section">
+                    <h1>School Name: {props.form.schoolName}</h1>
+                    <h1>Study Title: {props.form.studyTitle}</h1>
+                    <h1>Study Period: From <span className="date">{props.form.studyDateFrom}</span> To <span className="date">{props.form.studyDateTo}</span></h1>
+                </div>
+                <div className="form-section">
+                    <h1>Previous Company: {props.form.companyName}</h1>
+                    <h1>Position Title: {props.form.positionTitle}</h1>
+                    <h1>Main Tasks: {props.form.mainTasks}</h1>
+                    <h1>Working Period: From <span className="date">{props.form.workingDateFrom}</span> To <span className="date">{props.form.workingDateTo}</span></h1>
+                </div>
+                
+            </div>
             <button onClick={editForm} className='edit-btn'>Edit</button>
         </div>
     )
